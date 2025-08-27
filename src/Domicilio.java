d57d8dff0e11e8e642e8e990e2706f9eef118ab1
Gonzalo Jeremias Jaime Guiñazu <131501955+GzaJai@users.noreply.github.com>
2025-08-27 12:47:43 -0300

    Refactorización de Domicilio para usar anotaciones Lombok

--- src/Domicilio.java
+++ src/Domicilio.java
@@ -1,27 +1,15 @@
 // Clase Domicilio (versión corregida)
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "residente") // Excluimos 'residente' de la función ToString
+@EqualsAndHashCode(exclude = "residente") // Excluimos 'residente' para evitar ciclos o referencias recursivas al calcular equals() y hashCode()
 public class Domicilio {
     private String calle;
     private int numero;
-    // No necesitamos una referencia a Persona para el propósito del toString()
-    // Aunque la tuviéramos para otra lógica, no la usaríamos en este método
     private Persona residente;
-
-    public Domicilio(String calle, int numero) {
-        this.calle = calle;
-        this.numero = numero;
-    }
-
-    // Asignar el residente después de la creación
-    public void setResidente(Persona residente) {
-        this.residente = residente;
-    }
-
-    // Método toString() corregido: no llama a Persona.toString()
-    @Override
-    public String toString() {
-        return "Domicilio{" +
-                "calle='" + calle + '\'' +
-                ", numero=" + numero +
-                '}';
-    }
 }
